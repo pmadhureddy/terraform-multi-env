867f90039169807e4172229cc9e656b3762d68a4
pmadhureddy <pmadhureddy1510@gmail.com>
2024-09-22 00:50:48 +0530

    Created multiple  environments using tfvars and workspaces

---
+++ workspaces/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+ backend "s3" {
+  bucket = "madhu-locking-state"
+  key = "workspaces"
+  region = "us-east-1"
+  dynamodb_table = "terraformlocking"
+} 
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
